e93825c6c480477b34cdbf4dda0b03c22fe3cd8c
salazarlj <salazarlj@gmail.com>
2022-06-20 23:26:18 -0600

    Polishing website, just details

--- script/script.js
+++ script/script.js
@@ -23,3 +23,14 @@ currentTime = `  ${d.getHours()}:${d.getMinutes()}:${d.getMilliseconds()}`;
 const selection = { month: "long", day: "numeric", year: "numeric" };
 upateDates.textContent =
   new Date().toLocaleDateString("en-US", selection) + currentTime;
+
+
+function ver(n) {
+    document.getElementById("subseccion1"+n).style.display="block";
+    alert("TESTING SOMETHING");
+    }
+    
+function ocultar(n) {
+    document.getElementById("subseccion1"+n).style.display="none";
+    alert("TESTING SOMETHING");
+    }
